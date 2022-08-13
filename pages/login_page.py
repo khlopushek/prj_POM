38fe500ccc0aadb5daad368b7c10a5b9f14aaeeb
khlopushek <tanya.piskovtseva@mail.ru>
2022-08-13 14:32:49 +0400

    course is finally finished :)

--- pages/login_page.py
+++ pages/login_page.py
@@ -29,3 +29,20 @@ class LoginPage(BasePage):
         self.register_button = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
         self.register_button.click()
         
+
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        self.browser_url = self.browser.current_url
+        assert "login" in self.browser_url, "Login link is not presented"
+
+    def should_be_login_form(self):
+        self.login_form = self.browser.find_element(*LoginPageLocators.LOGIN_CSS)
+        assert EC.presence_of_element_located(self.login_form), "Login form is not presented"
+
+    def should_be_register_form(self):
+        self.register_form = self.browser.find_element(*LoginPageLocators.REGISTER_CSS)
+        assert EC.presence_of_element_located(self.register_form), "Register form is not presented" 
--- test_product_page.py
+++ test_product_page.py
@@ -13,75 +13,75 @@ from .pages.basket_page import BasketPage
 from .pages.locators import BasketPageLocators
 
 
-#def test_guest_can_go_to_login_page(browser):
-#    page = MainPage(browser, MainPageLinks.link)
-#    page.open()
-#    page.go_to_login_page()
-#    login_page = LoginPage(browser, browser.current_url)
-#    login_page.should_be_login_page()
+def test_guest_can_go_to_login_page(browser):
+    page = MainPage(browser, MainPageLinks.link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
-#def test_guest_should_see_login_link(browser):
-#    page = MainPage(browser, MainPageLinks.link)
-#    page.open()
-#    page.should_be_login_link()
-
-
-#def test_guest_can_add_product_to_basket(browser):
-#    page = MainPage(browser, MainPageLinks.link_promo) 
-#    page.open()
-#    product_page = ProductPage(browser, browser.current_url)
-#    product_page.add_to_basket()
-#    product_page.solve_quiz_and_get_code()
-#    product_page.should_be_valid_message()
-#    product_page.should_be_item_price()
-
-#@pytest.mark.xfail
-#def test_guest_cant_see_success_message_after_adding_product_to_basket(browser): 
-#    page = MainPage(browser, MainPageLinks.link_promo) 
-#    page.open()
-#    product_page = ProductPage(browser, browser.current_url)
-#    product_page.add_to_basket()
-#    assert product_page.is_not_element_present(*ProductPageLocators.MESSAGE_CSS), \
-#      "Success message is presented, but should not be"
+def test_guest_should_see_login_link(browser):
+    page = MainPage(browser, MainPageLinks.link)
+    page.open()
+    page.should_be_login_link()
 
 
-#def test_guest_cant_see_success_message(browser): 
-#    page = MainPage(browser, MainPageLinks.link_promo) 
-#    page.open()
-#    assert page.is_not_element_present(*ProductPageLocators.MESSAGE_CSS), \
-#      "Success message is presented, but should not be"
+def test_guest_can_add_product_to_basket(browser):
+    page = MainPage(browser, MainPageLinks.link_promo) 
+    page.open()
+    product_page = ProductPage(browser, browser.current_url)
+    product_page.add_to_basket()
+    product_page.solve_quiz_and_get_code()
+    product_page.should_be_valid_message()
+    product_page.should_be_item_price()
 
-#@pytest.mark.xfail
-#def test_message_disappeared_after_adding_product_to_basket(browser):
-#    page = MainPage(browser, MainPageLinks.link_promo) 
-#    page.open()
-#    product_page = ProductPage(browser, browser.current_url)
-#    product_page.add_to_basket()
-#    assert product_page.is_disappeared(*ProductPageLocators.MESSAGE_CSS), \
-#      "Success message is presented, but should not be"    
-
-#def test_guest_should_see_login_link_on_product_page(browser):
-#    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-#    page = ProductPage(browser, link)
-#    page.open()
-#    page.should_be_login_link()
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser): 
+    page = MainPage(browser, MainPageLinks.link_promo) 
+    page.open()
+    product_page = ProductPage(browser, browser.current_url)
+    product_page.add_to_basket()
+    assert product_page.is_not_element_present(*ProductPageLocators.MESSAGE_CSS), \
+      "Success message is presented, but should not be"
 
 
-#def test_guest_can_go_to_login_page_from_product_page(browser):
-#    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-#    page = ProductPage(browser, link)
-#    page.open()
-#    page.go_to_login_page()
+def test_guest_cant_see_success_message(browser): 
+    page = MainPage(browser, MainPageLinks.link_promo) 
+    page.open()
+    assert page.is_not_element_present(*ProductPageLocators.MESSAGE_CSS), \
+      "Success message is presented, but should not be"
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = MainPage(browser, MainPageLinks.link_promo) 
+    page.open()
+    product_page = ProductPage(browser, browser.current_url)
+    product_page.add_to_basket()
+    assert product_page.is_disappeared(*ProductPageLocators.MESSAGE_CSS), \
+      "Success message is presented, but should not be"    
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
 
-#def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-#    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-#    page = BasePage(browser, link)
-#    page.open()
-#    page.go_to_basket()
-#    page_basket = BasketPage(browser, browser.current_url)
-#    page_basket.should_be_empty_basket()
-#    page_basket.should_be_no_products()
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = BasePage(browser, link)
+    page.open()
+    page.go_to_basket()
+    page_basket = BasketPage(browser, browser.current_url)
+    page_basket.should_be_empty_basket()
+    page_basket.should_be_no_products()
 
 @pytest.mark.login_user
 class TestUserAddToBasketFromProductPage():
@@ -96,6 +96,7 @@ class TestUserAddToBasketFromProductPage():
         password = "eeelllkkkkkk"
         self.register.register_new_user(email, password)
         self.reg_page.should_be_authorized_user()
+
 
     def test_user_cant_see_success_message(self, browser): 
         page = MainPage(browser, MainPageLinks.link_promo) 
